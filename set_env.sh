33de4d705a50e6aa06177b7186de0887d1f912d4
Jack Featherstone <jfeatherstone123@gmail.com>
2024-11-04 15:52:13 +0900

    Removed data files

--- set_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Set the environment variables for using the spotify API
-# Usage: source set_env.sh
-
-export SPOTIFY_USERNAME="Jack5225x"
-
-export SPOTIFY_ID="724974497d0b47ea9511936f005db8f3"
-
-export SPOTIFY_SECRET="65c5f479688b4e7082d2eb41a15a32cd"
